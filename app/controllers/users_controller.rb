37cdd68a588d3206f2a53e939891f0833ff355d5
Brad Carlson <b2carlso@yahoo.com>
2018-03-11 20:46:13 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,15 @@
 class UsersController < ApplicationController
   def new
   end
+
+  def create
+    @user = User.new(user_params)
+
+    if !@user.save || params[:user][:name].empty?
+      redirect_to signup_path
+    else
+      session[:user_id] = @user.id
+      redirect_to welcome_index_path
+    end
+  end
 end
